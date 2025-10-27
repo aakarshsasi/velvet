925fae34923c48b015503811e865f360dc7f2efb
KR4010 <kreet4d@gmail.com>
2025-10-27 12:52:26 +0530

    trying to create env file before bable config

--- src/lib/RevenueCatManager.ts
+++ src/lib/RevenueCatManager.ts
@@ -1,10 +1,10 @@
 import { REVENUECAT_API_KEY } from '@env';
 import { Platform } from 'react-native';
 import Purchases, {
-    CustomerInfo,
-    LOG_LEVEL,
-    PurchasesOfferings,
-    PurchasesPackage,
+  CustomerInfo,
+  LOG_LEVEL,
+  PurchasesOfferings,
+  PurchasesPackage,
 } from 'react-native-purchases';
 
 // HARDCODED API KEY FOR DEBUGGING (will use this as fallback)
